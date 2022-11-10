43ee8785a2de99da20ec5175ec93b58113d6f4cb
Jake <manojjakcin.2001@gmail.com>
2022-11-10 10:43:23 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,58 @@
+# for i in reversed(range(6)):
+#     print("*"*i)
+
+# for i in range(6):
+#     print("* "*i)
+
+
+# Pyramid
+# n=6
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end='')
+
+#     for j in range(i+1):
+#         print("*",end='')
+
+#     for j in range(i):
+#         print("*",end='')
+#     print()
+
+#Reverse Pyramid
+# n=6
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end='')
+
+#     for j in range(i,n):
+#         print("*",end='')
+
+#     for j in range(i,n-1):
+#         print("*",end='')
+#     print()
+
+#Diamond
+
+# n=6
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print(" ",end='')
+
+#     for j in range(i+1):
+#         print("*",end='')
+
+#     for j in range(i):
+#         print("*",end='')
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end='')
+
+#     for j in range(i,n):
+#         print("*",end='')
+
+#     for j in range(i,n-1):
+#         print("*",end='')
+#     print()
+
